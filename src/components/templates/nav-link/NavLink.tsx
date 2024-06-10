5df647ae2db18716d51eae65294ec5ba915b70ef
ALLECHEN <43279583+PogChan@users.noreply.github.com>
2024-06-09 23:47:48 -0400

    drop down done  but wrong styling

--- src/components/templates/nav-link/NavLink.tsx
+++ src/components/templates/nav-link/NavLink.tsx
@@ -1,7 +1,8 @@
 // Libs
-import React, { FC, memo } from 'react'
+import React, { FC, memo, useState } from 'react'
 import styles from './nav-link.module.scss'
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 // Interface
 interface INavLink {
@@ -9,6 +10,10 @@ interface INavLink {
     | {
         name: string
         href: string
+        dropdown?: {
+          name: string
+          href: string
+        }[]
       }[]
     | null
     | undefined
@@ -19,19 +24,38 @@ interface INavLink {
 const NavLink: FC<INavLink> = ({ navLinks, variant = 'row' }) => {
   // Router
   const { push, pathname } = useRouter()
+  const [showDropdown, setShowDropdown] = useState(false)
+
   // Return
   return (
     <ul className={`${styles.nav_link} ${variant === 'column' ? styles.column : ''}`}>
       {navLinks &&
-        navLinks?.map((item, i) => (
+        navLinks.map((item, i) => (
           <li
             key={`${item.name} ${i}`}
             className={`${styles.nav_link__item} ${variant === 'column' ? styles.column : ''} ${
               pathname === item.href ? styles.active : ''
             }`}
-            onClick={() => push(item.href)}
+            onMouseEnter={() => setShowDropdown(true)}
+            onMouseLeave={() => setShowDropdown(false)}
           >
             {item.name}
+            {item.dropdown && (
+              <div
+                className='dropdown-container'
+                style={{ display: showDropdown ? 'block' : 'none' }}
+              >
+                <ul className='dropdown'>
+                  {item.dropdown.map((dropdownItem, dropdownIndex) => (
+                    <li key={dropdownIndex}>
+                      <Link href={dropdownItem.href} as={dropdownItem.href}>
+                        <a>{dropdownItem.name}</a>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
           </li>
         ))}
     </ul>
